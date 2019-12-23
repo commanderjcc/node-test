795690eeaeca2fb21765957bc566f85eb4d2ce4e
Josh <Josh.christensen48@masonohioschools.com>
2019-12-22 22:54:58 -0500

    add env vars

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const hostname = '127.0.0.1';
+const hostname = process.env.URL;
 const port = 3000;
 
 const server = http.createServer((req, res) => {
